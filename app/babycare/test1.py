a481c94782494e81d2cb82e533da5b5534c8426b
zhixiang pan <1906390603@qq.com>
2023-12-11 18:22:03 +0800

    update

--- app/babycare/test1.py
+++ app/babycare/test1.py
@@ -1,28 +1,46 @@
 import cv2
 import time
+import paho.mqtt.client as mqtt
+import base64
+
+MQTT_SERVER = "10.11.12.173"
+IMAGE_W = 640
+IMAGE_H = 480
 
 cap = cv2.VideoCapture('/dev/video0')  # 使用你提供的设备路径
-
-# 定义编解码器并创建VideoWriter对象
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 # 记录起始时间
 start_time = time.time()
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret:
-        # 将帧写入文件
-        out.write(frame)
+# Initialize the MQTT client
+client = mqtt.Client()
+client.connect(MQTT_SERVER, 1883, 60)
 
-        # 如果录制了10秒钟，就跳出循环
-        if time.time() - start_time > 10:
+try:
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret:
+            # Convert the frame to JPEG
+            ret, jpeg = cv2.imencode('.jpg', frame)
+
+            if not ret:
+                raise Exception('Could not encode image!')
+
+            b64_string = base64.b64encode(jpeg.tobytes()).decode('utf-8')
+
+            # Publish the encoded string via MQTT
+            client.publish("robot/camera/image_raw", b64_string)
+            print("sent a image to mqtt server")
+
+            # 如果录制了10秒钟，就跳出循环
+            if time.time() - start_time > 10:
+                break
+        else:
             break
-    else:
-        break
-
-# 完成后释放所有内容
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+except KeyboardInterrupt:
+    print("KeyboardInterrupt")
+finally:
+    # 完成后释放所有内容
+    cap.release()
+    cv2.destroyAllWindows()
+    print("已完成录像")
