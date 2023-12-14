dd278ca0609dfe00cc2ae7f03c85111c3cf0e495
yexia553 <1906390603@qq.com>
2023-12-14 07:43:07 -0800

    update

--- app/babycare/camera_websocket.py
+++ app/babycare/camera_websocket.py
@@ -12,9 +12,9 @@ async def send_frame(uri):
 
     try:
         async with websockets.connect(uri) as websocket:
-            cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-            prev_frame_time = 0
+            cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+            cap.set(cv2.CAP_PROP_FPS, 30)
             while True:
                 ret, frame = cap.read()
                 if not ret:
@@ -25,13 +25,8 @@ async def send_frame(uri):
                 if not ret:
                     print("Could not encode frame to JPEG")
                     break
+                await websocket.send(jpeg.tobytes())
 
-                # 帧率控制，每秒30帧
-                new_frame_time = time.time()
-                if new_frame_time - prev_frame_time > 1.0 / 30:
-                    # 发送 JPEG 二进制数据
-                    await websocket.send(jpeg.tobytes())
-                    prev_frame_time = new_frame_time
     finally:
         cap.release()
 
