3973a646740b086604775507805400b79bf0ff0c
yexia553 <1906390603@qq.com>
2023-12-16 05:45:12 -0800

    update

--- app/babycare/test4.py
+++ app/babycare/test4.py
@@ -9,13 +9,13 @@ url = "http://10.11.12.173:8000/api/monitor/hlsendpoint/"
 
 while True:
     for filename in os.listdir(directory):
-        if filename.endswith(".ts"):
+        if filename.endswith(".ts") or filename.endswith(".m3u8"):
             filepath = os.path.join(directory, filename)
             with open(filepath, "rb") as f:
                 files = {"file": (filename, f)}
-                response = requests.post(url, data=files)
+                response = requests.put(url, data=files)
                 # handle the response, e.g., check if it's successful
             # delete the file after it's sent
             os.remove(filepath)
     # sleep for a while before the next round
-    time.sleep(1)
+    time.sleep(0.5)
