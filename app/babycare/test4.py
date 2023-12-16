2d1795503f940cac45eeb35030ed0e99e1e9558d
yexia553 <1906390603@qq.com>
2023-12-16 05:19:40 -0800

    update

---
+++ app/babycare/test4.py
@@ -0,0 +1,21 @@
+import requests
+import os
+import time
+
+# the directory where the HLS segments and m3u8 file are saved
+directory = "/root/workspace/originbot_home_assistant/app/babycare/"
+# the URL of your Django server
+url = "http://10.11.12.173:8000/api/monitor/hlsendpoint/"
+
+while True:
+    for filename in os.listdir(directory):
+        if filename.endswith(".ts"):
+            filepath = os.path.join(directory, filename)
+            with open(filepath, "rb") as f:
+                files = {"file": (filename, f)}
+                response = requests.post(url, data=files)
+                # handle the response, e.g., check if it's successful
+            # delete the file after it's sent
+            os.remove(filepath)
+    # sleep for a while before the next round
+    time.sleep(1)
