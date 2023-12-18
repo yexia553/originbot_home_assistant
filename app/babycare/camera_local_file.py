8a9bc0daf832ed5040032aedac0367c86062a367
yexia553 <1906390603@qq.com>
2023-12-18 05:36:07 -0800

    update

--- app/babycare/camera_local_file.py
+++ app/babycare/camera_local_file.py
@@ -14,28 +14,30 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 # 获取当前时间
 now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8)))
 
-# 循环获取视频帧并保存
-while True:
-    # 获取一帧视频
-    ret, frame = cap.read()
-    if not ret:
-        break
+try:
+    # 循环获取视频帧并保存
+    while True:
+        # 获取一帧视频
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # 生成文件名
-    filename = now.strftime("%Y-%m-%d_%H-%M-%S.mp4")
+        # 生成文件名
+        filename = now.strftime("%Y-%m-%d_%H-%M-%S.avi")
 
-    # 保存视频帧
-    out = cv2.VideoWriter(
-        filename,
-        cv2.VideoWriter_fourcc("M", "P", "4", "V"),
-        30,
-        (frame.shape[1], frame.shape[0]),
-    )
-    out.write(frame)
-    out.release()
+        # 保存视频帧
+        out = cv2.VideoWriter(
+            filename,
+            cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+            30,
+            (frame.shape[1], frame.shape[0]),
+        )
+        out.write(frame)
+        out.release()
 
-    # 休眠10秒
-    time.sleep(10)
+        # 休眠10秒
+        time.sleep(10)
 
-# 释放摄像头
-cap.release()
+finally:
+    # 释放摄像头
+    cap.release()
