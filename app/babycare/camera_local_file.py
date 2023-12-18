05e9421cde626ab435197c18cb1d5cfe7570cf36
yexia553 <1906390603@qq.com>
2023-12-18 04:45:20 -0800

    update

--- app/babycare/camera_local_file.py
+++ app/babycare/camera_local_file.py
@@ -1,47 +1,58 @@
-"""
-在本地生成视频文件
-"""
 import cv2
 import time
+import requests
+import threading
 from datetime import datetime
 
-# 定义帧率和持续时间
-framerate = 30.0
-duration = 10
 
-# 定义编解码器并创建 VideoWriter 对象
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-out = None
+def send_video(file_path):
+    url = "http://example.com/upload"
+    with open(file_path, "rb") as f:
+        files = {"file": f}
+        response = requests.post(url, files=files)
+        if response.status_code != 200:
+            print("Failed to send video: {}".format(response.text))
 
-cap = cv2.VideoCapture("/dev/video0")
 
-# 设置分辨率
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cap.set(cv2.CAP_PROP_FPS, framerate)
-
-start_time = time.time()
-
-while cap.isOpened():
+def record_video(cap, out, start_time, duration, fourcc, framerate):
     ret, frame = cap.read()
-    if ret == True:
-        # 判断是否需要创建新的 VideoWriter 对象（即是否已经过了 10 秒）
+    if ret:
         if time.time() - start_time >= duration:
             if out is not None:
                 out.release()
+                # threading.Thread(
+                #     target=send_video, args=("{}.avi".format(current_time),)
+                # ).start()
             current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             out = cv2.VideoWriter(
                 "{}.avi".format(current_time), fourcc, framerate, (640, 480)
-            )
+         s   )
             start_time = time.time()
-        # 写入帧
         out.write(frame)
-    else:
-        break
+    return out, start_time
 
-# 释放资源
-cap.release()
-if out is not None:
-    out.release()
 
-cv2.destroyAllWindows()
+def main():
+    framerate = 60.0
+    duration = 10
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = None
+
+    cap = cv2.VideoCapture("/dev/video0")
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FPS, framerate)
+
+    start_time = time.time()
+    while cap.isOpened():
+        out, start_time = record_video(
+            cap, out, start_time, duration, fourcc, framerate
+        )
+    cap.release()
+    if out is not None:
+        out.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
