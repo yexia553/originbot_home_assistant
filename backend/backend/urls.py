c2831b4a49931a9a4ab5e57457fed43e77f165f6
yexia553 <1906390603@qq.com>
2023-12-19 05:33:41 -0800

    update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -26,3 +28,6 @@ urlpatterns = [
     path("api/voice_control/", include("voice_control.urls"), name="voice_control"),
     path("api/monitor/", include("monitor.urls"), name="monitor"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
